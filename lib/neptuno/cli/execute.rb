86468d62f7f523269483e6a9b79bc57743beb9d1
Joseph Groves <joseph.groves@apptegy.com>
2023-03-14 12:32:51 -0600

    fix: simplify exec implementation

--- lib/neptuno/cli/execute.rb
+++ lib/neptuno/cli/execute.rb
@@ -6,14 +6,14 @@ module Neptuno
     class Execute < Neptuno::CLI::Base
       include TTY::File
       include TTY::Config
+      
+      desc "Execute command inside of container"
 
-      desc "Execute service script"
-
-      def call(**options)
-        service_to, *command = options[:args]
-
-        command_service_to('execute', service_as_args: command.empty? ? service_to : nil) do |service, _project|
-          system("cd #{neptuno_path} && docker compose exec #{service} #{command}")
+      def call(services: [],**options)
+        command_service_to('execute', service_as_args: services) do |service, _project|
+          puts "Executing #{options[:args][-1]} inside of #{service} container"
+          system("cd #{neptuno_path} && docker compose exec #{service} ash -c \"#{options[:args][-1]}\"")
+          # TODO: Add support for referencing procs as executable commands with exec
         end
       end
     end
