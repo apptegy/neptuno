105fbaaaf53a1dc666c01d7e437cc2cddc4b8ed3
Roberto Plancarte <roberto.plancarte@apptegy.com>
2023-10-02 15:15:03 -0600

    allow build arguments to docker build

--- lib/neptuno/docker/build.rb
+++ lib/neptuno/docker/build.rb
@@ -8,17 +8,29 @@ module Neptuno
 
       option :all, type: :boolean, default: false, desc: 'Run on all services'
       argument :services, type: :array, required: false, desc: 'Optional list of services'
+      option :options, required: false, desc: 'Optional string of options passed to build'
 
       def call(services: [], **options)
         command_services_to('build', all: options.fetch(:all), services_as_args: services) do |services|
           make_service_files(services)
+          opts = options.fetch(:options, nil) || get_gpr
           services.each do |service|
             puts '********************'
             puts "building #{service}"
             puts '********************'
-            system("cd #{neptuno_path} && docker compose build #{service}")
+            system("cd #{neptuno_path} && docker compose build #{opts} #{service}")
           end
         end
+      end
+
+      def get_gpr
+        begin
+          lines = File.readlines("#{Dir.home}/.bundle/config")
+        rescue Errno::ENOENT
+          return
+        end
+        gpr = lines&.select { |line| line[/BUNDLE_RUBYGEMS__PKG__GITHUB__COM/] }&.last&.split(' ')&.last
+        "--build-arg gpr=#{gpr}".gsub('\"', '') if gpr
       end
     end
   end
--- lib/neptuno/docker/restart.rb
+++ lib/neptuno/docker/restart.rb
@@ -4,19 +4,31 @@ module Neptuno
   module Docker
     # Restart docker containers for Neptuno project
     class Restart < Neptuno::CLI::Base
-      desc "Docker: Rebuild and restart docker containers for current project"
+      desc 'Docker: Rebuild and restart docker containers for current project'
 
-      option :all, type: :boolean, default: false, desc: "Run on all services"
-      argument :services, type: :array, required: false, desc: "Optional list of services"
+      option :all, type: :boolean, default: false, desc: 'Run on all services'
+      argument :services, type: :array, required: false, desc: 'Optional list of services'
+      option :options, required: false, desc: 'Optional string of options passed to build'
 
       def call(services: [], **options)
-        command_services_to("restart", all: options.fetch(:all), services_as_args: services) do |services|
+        command_services_to('restart', all: options.fetch(:all), services_as_args: services) do |services|
           make_service_files(services)
-          system("cd #{neptuno_path} && docker compose stop -t 0 #{services.join(" ")}")
-          system("cd #{neptuno_path} && docker compose rm -f #{services.join(" ")}")
-          system("cd #{neptuno_path} && docker compose build #{services.join(" ")}")
-          system("cd #{neptuno_path} && docker compose up -d #{services.join(" ")}")
+          opts = options.fetch(:options, nil) || get_gpr
+          system("cd #{neptuno_path} && docker compose stop -t 0 #{services.join(' ')}")
+          system("cd #{neptuno_path} && docker compose rm -f #{services.join(' ')}")
+          system("cd #{neptuno_path} && docker compose build #{opts} #{services.join(' ')}")
+          system("cd #{neptuno_path} && docker compose up -d #{services.join(' ')}")
         end
+      end
+
+      def get_gpr
+        begin
+          lines = File.readlines("#{Dir.home}/.bundle/config")
+        rescue Errno::ENOENT
+          return
+        end
+        gpr = lines&.select { |line| line[/BUNDLE_RUBYGEMS__PKG__GITHUB__COM/] }&.last&.split(' ')&.last
+        "--build-arg gpr=#{gpr}".gsub('\"', '') if gpr
       end
     end
   end
